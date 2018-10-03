564edae7e47afc9feabeed5e15250465592530d0
Philip Drozd <me@philipdrozd.com>
2018-10-03 13:52:27 +0200

    switch to angular shopping cart software and customize first steps

---
+++ zeitlos-app-deprecated/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SliderComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ zeitlos-app/src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyD7k-X7VG8CZVMTZOoiZFk_8fEeJhCuxQU",
+  authDomain: "zeitlos-1533472550392.firebaseapp.com",
+  databaseURL: "https://zeitlos-1533472550392.firebaseio.com",
+  projectId: "zeitlos-1533472550392",
+  storageBucket: "zeitlos-1533472550392.appspot.com",
+  messagingSenderId: "383738104850"
+};
